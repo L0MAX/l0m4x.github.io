1575d03047a0b46a86e9252a0ae9c26776b6eec8
Edzebubu Ankah <edzebubu@Edzebubus-MBP.mshome.net>
2022-04-05 20:25:04 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Resume - Donatus Edzebubu Ankah</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Donatus Edzebubu Ankah">
+    <link rel="shortcut-icon" href="{{asset('assets/favicon.ico')}}">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    <!-- FontAwesome JS-->
+    <script defer src="{{asset('assets/fontawesome/js/all.min.js')}}"></script>
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="{{asset('assets/css/devresume.css')}}">
+
+</head>
+
+<body>
+
+<div class="main-wrapper">
+    <div class="container px-3 px-lg-5">
+        <article class="resume-wrapper mx-auto theme-bg-light p-5 mb-5 my-5 shadow-lg">
+            <div class="resume-header">
+                <div class="row align-items-center">
+                    <div class="resume-title col-12 col-md-6 col-lg-8 col-xl-9">
+                        <h2 class="resume-name mb-0 text-uppercase">Donatus Edzebubu Ankah</h2>
+                        <div class="resume-tagline mb-3 mb-md-0">Software Engineer</div>
+                    </div><!--//resume-title-->
+                    <div class="resume-contact col-12 col-md-6 col-lg-4 col-xl-3">
+                        <ul class="list-unstyled mb-0">
+                            <li class="mb-2"><i class="fas fa-phone-square fa-fw fa-lg mr-2 "></i><a class="resume-link" href="tel:+233544792829">+233 544 792 829</a></li>
+                            <li class="mb-2"><i class="fas fa-envelope-square fa-fw fa-lg mr-2"></i><a class="resume-link" href="mailto:ankahdonatus@gmail.com">ankahdonatus@gmail.com</a></li>
+                            <li class="mb-2"><i class="fas fa-globe fa-fw fa-lg mr-2"></i><a class="resume-link" href="https://www.donatus.foxnewsgh.com" target="_blank">www.donatus.foxnewsgh.com</a></li>
+                            <li class="mb-0"><i class="fas fa-map-marker-alt fa-fw fa-lg mr-2"></i>Accra Ghana</li>
+                        </ul>
+                    </div><!--//resume-contact-->
+                </div><!--//row-->
+
+            </div><!--//resume-header-->
+            <hr>
+            <div class="resume-intro py-3">
+                <div class="media flex-column flex-md-row align-items-center">
+                    <img class="dona resume-profile-image mb-3 mb-md-0 mr-md-5 ml-md-0 rounded mx-auto" src="{{asset('assets/images/donatus.png')}}" alt="image">
+                    <div class="media-body text-left">
+                        <p class="mb-0">Enthusiastic software engineer. <a class="theme-link" href="#" target="_blank"></a>
+                            with 5+ years experience participating in the complete product development lifecycle of successfully launched applications.
+                            Eager to join your company to deliver mission-critical technology and business solutions to Fortune 500 companies and some of the
+                            most recognized brands on the planet.</p>
+                    </div><!--//media-body-->
+                </div>
+            </div><!--//resume-intro-->
+            <hr>
+            <div class="resume-body">
+                <div class="row">
+                    <div class="resume-main col-12 col-lg-8 col-xl-9 pr-0 pr-lg-5">
+                        <section class="work-section py-3">
+                            <h3 class="text-uppercase resume-section-heading mb-4">Work Experiences</h3>
+                            <div class="item mb-3">
+                                <div class="item-heading row align-items-center mb-2">
+                                    <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Lead Software Developer</h4>
+                                    <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Insight Network IT Sol. | 2019 - Present</div>
+
+                                </div>
+                                <div class="item-content">
+                                    <p> I am responsible for directing the development team in the design, development,
+                                        coding, testing and debugging of applications. I also coordinate the development
+                                        staff’s schedule and ensure effective communication between team members and
+                                        other IT functional areas. Additionally, I provide feedback and suggestions
+                                        for process and product improvement</p>
+                                    <!--										<ul class="resume-list">-->
+                                    <!--											<li>.</li>-->
+                                    <!--											<li></li>-->
+                                    <!--										</ul>-->
+                                </div>
+                            </div><!--//item-->
+                            <div class="item mb-3">
+                                <div class="item-heading row align-items-center mb-2">
+                                    <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Project Supervisor</h4>
+                                    <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Insight Network IT Sol. | MAR 2019 - JUN 2019</div>
+
+                                </div>
+                                <div class="item-content">
+                                    <p>Develop and implement project work plans and review as appropriate to meet changing needs and requirements.
+                                        Evaluate project and recommend report to identify issues and opportunities to improve performance going forward.
+                                        Participate actively as projects team member to improve department efficiency, reduce costs and assist in
+                                        improvement of product through customer experiences.</p>
+                                    <!--										<ul class="resume-list">-->
+                                    <!--											<li>.</li>-->
+                                    <!--											<li>.</li>-->
+                                    <!--											<li>.</li>-->
+                                    <!--										</ul>-->
+                                </div>
+                            </div><!--//item-->
+                            <div class="item">
+                                <div class="item-heading row align-items-center mb-2">
+                                    <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Digital Banking Intern</h4>
+                                    <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Stanbic Bank Gh | JUN 201 - AUG 2018</div>
+
+                                </div>
+                                <div class="item-content">
+                                    <p> Worked at the digital banking department of Stanbic bank as an intern.</p>
+                                    <ul class="resume-list">
+                                        <li>Developed a loyalty rewarding system that rewards customers with points based on their transactions.</li>
+                                        <li>Was part of the team that developed SIVA (Stanbic Intelligent Virtual Assistant) chat bot for banking transactions.</li>
+                                        <li>Part of the digital marketing department.</li>
+                                    </ul>
+                                </div>
+                            </div><!--//item-->
+
+                        </section><!--//work-section-->
+
+
+                        <section class="project-section py-3">
+                            <h3 class="text-uppercase resume-section-heading mb-4">Projects</h3>
+                            <div class="item mb-3">
+                                <div class="item-heading row align-items-center mb-2">
+                                    <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">USSD Bus Ticketing system</h4>
+                                    <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Open Source</div>
+
+                                </div>
+                                <div class="item-content">
+                                    <p>Designed, developed and successfully tested a USSD <a href="https://github.com/L0MAX/USSD-Bus-Ticketing-System" class="theme-link" target="_blank">based bus ticketing
+                                            system</a> hooked with Africa is talking USSD API.</p>
+
+
+                                </div>
+                            </div><!--//item-->
+                            <div class="item">
+                                <div class="item-heading row align-items-center mb-2">
+                                    <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Covid19 Tracker</h4>
+                                    <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Open Source</div>
+
+                                </div>
+                                <div class="item-content">
+                                    <p>Designed and Developed an <a href="https://github.com/L0MAX/TrackCovid19" class="theme-link" target="_blank">Android Based App </a>for tracking the infections,Death,recovery information of affected countries</p>
+
+                                </div>
+                            </div><!--//item-->
+                            <div class="item">
+                                <div class="item-heading row align-items-center mb-2">
+                                    <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">HTU Market </h4>
+                                    <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">E-Commerce</div>
+
+                                </div>
+                                <div class="item-content">
+                                    <p>Built an E-commerce site for Entrepreneurship department of HO Technical University, known as
+                                        <a href="https://htumarket.com" class="theme-link" target="_blank">HTU Market</a> for Business transactions</p>
+
+                                </div>
+                                <div class="item-heading row align-items-center mb-2">
+                                    <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Insight DMS </h4>
+                                    <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Private</div>
+
+                                </div>
+                                <div class="item-content">
+                                    <p>Built an Electronic document management system for internal use.<a href="https://htumarket.com" class="theme-link" target="_blank"></a>
+                                        this helps reduce the increase use of paper</p>
+
+                                </div>
+                            </div><!--//item-->
+                        </section><!--//project-section-->
+                    </div><!--//resume-main-->
+                    <aside class="resume-aside col-12 col-lg-4 col-xl-3 px-lg-4 pb-lg-4">
+                        <section class="skills-section py-3">
+                            <h3 class="text-uppercase resume-section-heading mb-4">Skills</h3>
+                            <div class="item">
+                                <h4 class="item-title">Technical</h4>
+                                <ul class="list-unstyled resume-skills-list">
+                                    <li class="mb-2">JavaScript</li>
+                                    <li class="mb-2">Python/PHP<li>
+                                    <li class="mb-2">Java</li>
+                                    <li class="mb-2">Visual Basic/ASP.NET</li>
+                                    <li class="mb-2">MySQL</li>
+                                    <li class="mb-2">Design and implement database structures</li>
+                                    <li>Lead and deliver complex software systems</li>
+                                </ul>
+                            </div><!--//item-->
+                            <div class="item">
+                                <h4 class="item-title">Professional</h4>
+                                <ul class="list-unstyled resume-skills-list">
+                                    <li class="mb-2">Effective communication</li>
+                                    <li class="mb-2">Team player<li>
+                                    <li class="mb-2">Strong problem solver</li>
+                                    <li>Good time management</li>
+                                </ul>
+                            </div><!--//item-->
+                        </section><!--//skills-section-->
+                        <section class="education-section py-3">
+                            <h3 class="text-uppercase resume-section-heading mb-4">Education</h3>
+                            <ul class="list-unstyled resume-education-list">
+                                <li class="mb-3">
+                                    <div class="resume-degree font-weight-bold">HND in Computer Science</div>
+                                    <div class="resume-degree-org text-muted">Ho Technical University Ho</div>
+                                    <div class="resume-degree-time text-muted">2016 - 2019</div>
+                                </li>
+                            </ul>
+                        </section><!--//education-section-->
+                        <section class="education-section py-3">
+                            <h3 class="text-uppercase resume-section-heading mb-4">Awards</h3>
+                            <ul class="list-unstyled resume-awards-list">
+                                <li class="mb-3">
+                                    <div class="font-weight-bold">Vodafone Special Awards</div>
+                                    <div class="text-muted">HackLab Foundation (2018)</div>
+                                </li>
+
+                            </ul>
+                        </section><!--//education-section-->
+                        <section class="skills-section py-3">
+                            <h3 class="text-uppercase resume-section-heading mb-4">Languages</h3>
+                            <ul class="list-unstyled resume-lang-list">
+                                <li class="mb-2">English <span class="text-muted">(Professional)</span></li>
+                                <li>Ewe <span class="text-muted">(Native)</span></li>
+                                <li>Twi <span class="text-muted"></span></li>
+                                <li>Sefwi <span class="text-muted"></span></li>
+                            </ul>
+                        </section><!--//certificates-section-->
+                        <section class="skills-section py-3">
+                            <h3 class="text-uppercase resume-section-heading mb-4">Interests</h3>
+                            <ul class="list-unstyled resume-interests-list mb-0">
+                                <li class="mb-2">Swimming</li>
+                                <li class="mb-2">Photography</li>
+                                <li class="mb-2">Football</li>
+                                <li>Travelling</li>
+                            </ul>
+                        </section><!--//certificates-section-->
+
+                    </aside><!--//resume-aside-->
+                </div><!--//row-->
+            </div><!--//resume-body-->
+            <hr>
+            <div class="resume-footer text-center">
+                <ul class="resume-social-list list-inline mx-auto mb-0 d-inline-block text-muted">
+                    <li class="list-inline-item mb-lg-0 mr-3"><a class="resume-link" href="https://github.com/L0MAX" target="_blank"><i class="fab fa-github-square fa-2x mr-2" data-fa-transform="down-4"></i><span class="d-none d-lg-inline-block text-muted">L0MAX Ankah Donatus</span></a></li>
+                    <li class="list-inline-item mb-lg-0 mr-3"><a class="resume-link" href="https://www.linkedin.com/in/ankah-donatus-42a726134/" target="_blank"><i class="fab fa-linkedin fa-2x mr-2" data-fa-transform="down-4"></i><span class="d-none d-lg-inline-block text-muted">ankah donatus</span></a></li>
+                    <li class="list-inline-item mb-lg-0 mr-lg-3"><a class="resume-link" href="https://twitter.com/Legendankah" target="_blank"><i class="fab fa-twitter-square fa-2x mr-2" data-fa-transform="down-4"></i><span class="d-none d-lg-inline-block text-muted">@legendankah</span></a></li>
+                </ul>
+            </div><!--//resume-footer-->
+        </article>
+
+    </div><!--//container-->
+
+    <footer class="footer text-center py-4">
+
+        <small class="copyright text-muted">Designed with <i class="fas fa-heart"></i> by <a class="theme-link" href="https://insightnet.voxgh.com" target="_blank">Ankah Donatus</a></small>
+    </footer>
+
+</div>
+
+</body>
+</html>
